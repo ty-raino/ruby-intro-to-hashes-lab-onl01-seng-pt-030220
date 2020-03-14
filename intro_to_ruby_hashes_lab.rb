01cde48db97360d56120d9a3c7f6318e27446e1d
Ty Raino <mstymetris@yahoo.com>
2020-03-14 08:04:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,10 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}
+	monopoly = {railroads:
+		{pieces: 4,
+			names: {},
+			rent_in_dollars: {one_piece_owned: 24}
  }
 }
 
